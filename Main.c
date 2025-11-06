8902bee392cb467d06675ea9a35c4004ef2cfdba
Ida Jakobsen <idax035c@gmail.com>
2025-11-06 14:58:48 +0100

    Færdiprogrammeret indtasting af ingredienser og printing

--- Main.c
+++ Main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 //En struct der gør at vi kan gemme ingredienser og pointe videre til den næste. (linked lists) 
 //En struct er en datatype man selv kan definere
@@ -9,8 +10,12 @@ typedef struct Node{
     struct Node *next; //pointer til næste node i den linkede liste
 }Node;
 
+//Prototyper
 void get_ingredient(char *ingrediens_input);
 Node* tilfoj_ingrediens_til_liste(char *ingrediens_input, Node *head);
+void free_list(Node *head);
+void print_list(Node *head);
+void capitalize(char *ingrediens_input);
 
 
 int main(void)
@@ -18,6 +23,7 @@ int main(void)
     Node *head = NULL; //Declares a pointer head der peger på en node struct. NULL initilieres den til så den ikke peger et sted hen endnu.
     char ingrediens_input[50];
 
+    //promt brugeren for ingredienser og gem dem i en liste.
     while(1)
     {
         get_ingredient(ingrediens_input);
@@ -25,8 +31,15 @@ int main(void)
             break;
         }
         
+        capitalize(ingrediens_input);
         head = tilfoj_ingrediens_til_liste(ingrediens_input, head);
     }
+
+    //Print listen
+    print_list(head);
+
+    //Frigørelse a hukommelse
+    free_list(head);
 
     return 0;
 }
@@ -47,7 +60,7 @@ Node* tilfoj_ingrediens_til_liste(char *ingrediens_input, Node *head){
 
    
     strcpy(n->ingredient, ingrediens_input);
-    //put the node in the list
+    //put noden i listen
     n->next = head; //n's næste felt er sat lig listen og indsættes derfor forrest i listen
     head = n; //listen bliver sat lig n som er adressen til noden og ingrediensen der er blevet givet af brugeren.
     return head;
@@ -55,7 +68,42 @@ Node* tilfoj_ingrediens_til_liste(char *ingrediens_input, Node *head){
 
 void get_ingredient(char *ingrediens_input){
     
-    printf("Indtast en ingrediens(exit for at afslutte): ");
+    printf("Indtast en ingrediens('exit' for at afslutte): ");
     //begrænser mængden af char's som kan blive sannet ind til at passe med størrelsen på vores array
     scanf("%49s", ingrediens_input);
+}
+
+void print_list(Node *head){
+    printf("Ingredienser indtastet:\n");
+    //Looper igennem listen og printer ingredienserne
+    while(head != NULL){
+        printf("%s\n",head->ingredient);
+        //sætter pointeren head til at pege på den næste i listen
+        head = head->next;
+    }
+}
+
+// Funktion der firgører den hukkomelse listen har brugt 
+void free_list(Node *head){
+    while(head != NULL){
+        Node *tmp = head;
+        head = head ->next;
+        free(tmp);
+    }
+}
+
+void capitalize(char *ingrediens_input){
+    //Konverterer det første bukstav til stort
+    ingrediens_input[0] = toupper(ingrediens_input[0]);
+
+    
+    int i = 1;
+    while(ingrediens_input[i] != '\0'){
+        //Tjekker om det resten af bukstaverne er med stort 
+        if(isupper(ingrediens_input[i])){
+            //Konverterer hvis bukstavet er stort
+            ingrediens_input[i] = tolower(ingrediens_input[i]);
+        }
+        i++;
+    }
 }
