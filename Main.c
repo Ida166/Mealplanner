7a6995e4701f4bd7f432db2019a978e0e49b68c0
Kristoffer-Christiansen <kristofferwcb@gmail.com>
2025-11-14 13:57:05 +0100

    Merging af de to branches

--- Main.c
+++ Main.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include "IngrediensIndtast.h"
 #include "KostPreference.h"
 
 int main(){
+
     printf("\nVelkommen til meal plan programmet!\n\n");
-
     listOptions();
-
     IngrediensIndtast_mainFunction();
 
     return 0;
+}
