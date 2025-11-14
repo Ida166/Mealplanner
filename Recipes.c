03b5bda301019aa83f51accc91c8cf4bf1a53d00
Ida Jakobsen <idax035c@gmail.com>
2025-11-14 17:29:58 +0100

    Add Recipes.c with recipe data

--- Recipes.c
+++ Recipes.c
@@ -3,9 +3,11 @@
 
 
 Recipe recipes_breakfast[] = {
-    {"Pancakes", 30, 2, 300, {"flour", "eggs", "milk", "sugar"}, "First mix the dry igredients and ad the weat ones. Then heat the pan and cook the pancakes til they are done.", 4},
-    {"Omelette", 30, 1, 350, {"eggs", "milk", "salt", "pepper"}, "First mix the ingredients and then cook them on the pan until the eggmass is bakaed", 4},
-    
+    {"Pancakes", 30, 2, 300, {"flour: 200g", "eggs: 1", "milk: 100ml", "sugar"}, "First mix the dry igredients and ad the weat ones. Then heat the pan and cook the pancakes til they are done.", 4},
+    {"Omelette", 30, 1, 350, {"eggs: 2", "milk: 100ml", "salt", "pepper"}, "First mix the ingredients and then cook them on the pan until the eggmass is bakaed", 4},
+    {"Skyr", 5, 1, 350, {"Skyr: 200g", "Fruit", "Oats"}, "First put the skyr in a bowl and add your toppings, fruit and oats", 3},
+    {"Oats", 3, 1, 400, {"Oats: 70g", "Cinnamon", "Apple", "Milk: 200g"}, "First add the oats to a bowl and pour milk in then add choped apples and drissle with cinnamon", 4},
+    {"Toast", 5, 1, 400, {"Toast", "Chease", "Ham"}, "Assemble the toast and grill it if wanted", 3},
 };
 int breakfastcount = 2;
 
