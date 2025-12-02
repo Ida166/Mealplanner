2fdfdcce7b25e3c82fcd41a51eec9d376e6b37c1
Ida166 <idax035c@gmail.com>
2025-12-02 11:22:21 +0100

    Update printmealplanner.c

--- printmealplanner.c
+++ printmealplanner.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include "Recipes.h" 
+
 
 int print_mealplan(int breakfastResultArray[7], int lunchResultArray[7], int dinnerResultArray[7]){
     //åbner filen
-    FILE *f = fopen("printmealplanner.csv", "w");
+    FILE *f = fopen("printmeal.csv", "w");
     if (!f){
         printf("Could not open file for writing.\n");
         return 1;
@@ -12,7 +14,7 @@ int print_mealplan(int breakfastResultArray[7], int lunchResultArray[7], int din
     fprintf(f, "Meal Plan Template\n");
 
     // Generer Kolonneoverskrifter
-    fprintf(f, "Day,Breakfast,Lunch,Dinner\n");
+    fprintf(f, "Day;Breakfast;Lunch;Dinner\n");
 
     // Dage i ugen
     const char *days[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", 
@@ -20,14 +22,46 @@ int print_mealplan(int breakfastResultArray[7], int lunchResultArray[7], int din
 
     //Fyld csv filen med opskrifterne
     for (int i = 0; i < 7; i++) {
+        Recipe breakfast = recipes_breakfast[breakfastResultArray[i]];
+        Recipe lunch = recipes_lunch[lunchResultArray[i]];
+        Recipe dinner = recipes_dinner[dinnerResultArray[i]];
         
-        fprintf(f, "%s,"%s","%s","%s"\n",
-        days[i],
-        recipes_breakfast[breakfastResultArray[i]].name,
-        recipes_lunch[lunchResultArray[i]].name,
-        recipes_dinner[dinnerResultArray[i]].name);
-    }
+         // Day column
+        fprintf(f, "%s;", days[i]);
 
+        // Breakfast column
+        fprintf(f, "\"%s, Time: %d, Portions: %d, Calories: %d, ", breakfast.name, breakfast.time, breakfast.portion, breakfast.calories);
+        fprintf(f, "Ingredients: ");
+        for(int j = 0; j < breakfast.ingredient_count; j++) {
+            fprintf(f, "%s, ", breakfast.ingredient[j]);
+        }
+        fprintf(f, "Procedure: %s\"", breakfast.procedure);
+        
+            
+        // Add semicolon before moving to lunch
+        fprintf(f, ";");    
+
+        // Lunch column
+        fprintf(f, "\"%s, Time: %d, Portions: %d, Calories: %d, ", lunch.name, lunch.time, lunch.portion, lunch.calories);
+        fprintf(f, "Ingredients: ");
+        for (int j = 0; j < lunch.ingredient_count; j++) {
+            fprintf(f, "%s, ", lunch.ingredient[j]);
+        }
+        fprintf(f, "Procedure: %s\"", lunch.procedure);
+
+        // Add semicolon before moving to dinner
+        fprintf(f, ";"); 
+
+        // Dinner column
+        fprintf(f, "\"%s, Time: %d, Portions: %d, Calories: %d, ", dinner.name, dinner.time, dinner.portion, dinner.calories);
+        fprintf(f, "Ingredients: ");
+        for (int j = 0; j < dinner.ingredient_count; j++) {
+            fprintf(f, "%s, ", dinner.ingredient[j]);
+        }
+        fprintf(f, "Procedure: %s\"\n", dinner.procedure);  // newline at end of row
+
+    }
     fclose(f);
     return 0;
 }
+
